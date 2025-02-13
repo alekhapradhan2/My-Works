34d8352573dcba42ddc746a67145d972865a9dd1
alekhpradhan2 <alekhpradhan18@gmail.com>
2025-02-13 13:04:34 +0530

    TokenImorter.java -> src/TokenExporter.java

--- src/TokenExporter.java
+++ src/TokenExporter.java
@@ -19,7 +19,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class TokenImorter {
+public class TokenExporter {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 		
